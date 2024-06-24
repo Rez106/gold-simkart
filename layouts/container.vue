051765e0fa39df82dacbe81ec61ef07f756dd316
Reza Darvishi <darvishireza8181@gmail.com>
2024-06-24 17:53:43 +0330

    add container, default, and main layouts

---
+++ layouts/container.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="max-w-[1320px] m-auto">
+    <slot />
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="min-w-full min-h-dvh bg-gradient-to-tr from-main-black-950 to-[#181818]"
+  >
+    <slot />
+  </div>
+</template>
+
+<style>
+  body {
+    font-family: IRANSans !important;
+    direction: rtl;
+  }
+  body::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+    background-color: #191919;
+  }
+
+  body::-webkit-scrollbar {
+    width: 8px;
+    background-color: #191919;
+  }
+
+  body::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: #ffda0a;
+  }
+</style>
