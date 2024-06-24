8b738311e6a239eeed09a3dc47106e6ef00c3215
Reza Darvishi <darvishireza8181@gmail.com>
2024-06-24 17:54:34 +0330

    add styles and tags to main layout

--- layouts/main.vue
+++ layouts/main.vue
@@ -1,5 +1,12 @@
 <template>
-  <main></main>
+  <main class="w-full flex gap-5 mt-6">
+    <aside class="w-[30%] h-20 bg-white">
+      <slot name="sidebar" />
+    </aside>
+    <div class="w-[70%] h-20 bg-white">
+      <slot name="page" />
+    </div>
+  </main>
 </template>
 
 <script setup></script>
