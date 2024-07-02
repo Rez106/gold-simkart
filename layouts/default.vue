b12a4770c536412eba6f745a0365fef65216683e
Reza Darvishi <darvishireza8181@gmail.com>
2024-07-02 20:46:13 +0330

    add IRANSans with persian number support

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,10 +7,31 @@
 </template>
 
 <style>
-  body {
-    font-family: IRANSans !important;
-    direction: rtl;
+  *,
+  *::before,
+  *::after {
+    scroll-behavior: smooth;
+    letter-spacing: 0;
   }
+
+  body {
+    direction: rtl;
+    font-family: IRANSans !important;
+    font-feature-settings: "ss02" !important;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  a,
+  span {
+    font-family: IRANSans !important;
+    font-feature-settings: "ss02" !important;
+  }
+
   body::-webkit-scrollbar-track {
     -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
     border-radius: 10px;
@@ -18,7 +39,7 @@
   }
 
   body::-webkit-scrollbar {
-    width: 8px;
+    width: 5px;
     background-color: #191919;
   }
 
@@ -26,5 +47,22 @@
     border-radius: 10px;
     -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
     background-color: #ffda0a;
+  }
+
+  div::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+    background-color: transparent;
+  }
+
+  div::-webkit-scrollbar {
+    width: 5px;
+    background-color: transparent;
+  }
+
+  div::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: #fff;
   }
 </style>
