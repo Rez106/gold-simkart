e30e325d048aa7fe2c54e3020b069650dab3543e
Reza Darvishi <darvishireza8181@gmail.com>
2024-07-02 21:05:18 +0330

    complete numbers output component for mobile

---
+++ components/mobile/NumberDetail.vue
@@ -0,0 +1,72 @@
+<template>
+  <div
+    class="flex flex-col items-center gap-2 rounded-b-lg duration-300 border-main-black-400 text-base max-small:text-xs"
+    :class="{
+      'h-0 overflow-hidden border-none opacity-0 bg-main-black-600': !isShow,
+      'max-[450px]:h-42  h-36 border-[1px] border-t-transparent p-2 px-2 bg-main-black-900':
+        isShow,
+    }"
+  >
+    <div
+      class="grid grid-cols-2 items-center text-center grid-rows-2 duration-150"
+      :class="{ 'opacity-0': !isShow }"
+    >
+      <div class="flex items-center gap-2 text-main-yellow-400 font-medium">
+        <span> اپراتور: </span>
+        <div class="bg-main-black-100 rounded-lg p-1">
+          <v-img
+            :src="'/images/' + num.operator_type + '.svg'"
+            width="30"
+            height="30"
+          />
+        </div>
+        <span class="text-white">
+          {{ operatorFormatter(num.operator_type) }}
+        </span>
+      </div>
+      <div class="text-main-yellow-400 font-medium">
+        <span> پیش‌ شماره: </span>
+        <span class="text-white">{{ num.preCode }}</span>
+      </div>
+
+      <div class="text-main-yellow-400 font-medium">
+        <span> شماره: </span>
+        <span class="text-white">{{ num.digits }}</span>
+      </div>
+      <div class="flex items-center gap-2 text-main-yellow-400 font-medium">
+        نوع شماره:
+        <span class="text-white">{{ num.category ? "اعتباری" : "دائمی" }}</span>
+      </div>
+    </div>
+    <v-btn
+      prepend-icon="mdi-phone-outgoing"
+      text="تماس برای خرید"
+      rounded="lg"
+      color="#222222"
+      class="w-fit self-center"
+      style="border: 1px solid #ffda0a; color: #ffda0a"
+      href="tel:+989010000550"
+    ></v-btn>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    num: Object,
+    isShow: Boolean,
+  });
+
+  const operatorFormatter = (optId) => {
+    return optId === 1
+      ? "همراه اول"
+      : optId === 2
+      ? "ایرانسل"
+      : optId === 3
+      ? "رایتل"
+      : optId === 4
+      ? "آپتل"
+      : "";
+  };
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/mobile/NumberItem.vue
@@ -0,0 +1,117 @@
+<template>
+  <div
+    class="flex items-center rounded-lg duration-200 justify-between border-[1px] border-main-black-400 py-3 text-base"
+    :class="{
+      '!rounded-b-none border-b-transparent bg-main-black-900': isShow,
+      'bg-main-black-600': !isShow,
+    }"
+  >
+    <div class="w-2/6 max-small:w-[43%] flex flex-col gap-2 pr-4">
+      <div class="w-full text-right">
+        <span
+          class="block text-main-black-300 font-semibold text-sm"
+          style="direction: ltr"
+        >
+          {{ num.mask_digit.split("-").join(" ") }}
+        </span>
+      </div>
+      <div class="flex items-center gap-2">
+        <span class="block text-main-black-300 font-semibold text-xs">
+          {{ statusFormatter(num.status) }}
+        </span>
+        <v-divider vertical />
+        <span class="block text-main-black-300 font-semibold text-xs">
+          نقد و اقساط
+        </span>
+      </div>
+    </div>
+
+    <div class="w-1/4 text-center">
+      <div
+        v-if="num.discount_amount > 0"
+        class="w-full flex items-center justify-center gap-3 text-center"
+      >
+        <div>
+          <span
+            class="block text-main-yellow-400 font-semibold"
+            style="direction: ltr"
+          >
+            {{ num.discount_amount }}
+          </span>
+          <span
+            class="block text-main-black-300 text-xs line-through"
+            style="direction: ltr"
+          >
+            {{ priceFormatter(num.price) }}
+          </span>
+        </div>
+        <span
+          class="block text-main-black-200 font-semibold bg-red-500 rounded-lg p-1 text-xs"
+          style="direction: ltr"
+        >
+          {{ discountPercent(num.price, num.discount_amount) }}
+        </span>
+      </div>
+      <div
+        v-else
+        class="w-full flex items-center justify-center gap-3 text-center"
+      >
+        <span
+          class="block text-main-black-300 font-semibold"
+          style="direction: ltr"
+        >
+          {{ priceFormatter(num.price) }}
+        </span>
+      </div>
+    </div>
+    <div class="w-1/4 text-center">
+      <span
+        class="font-semibold p-1 rounded-lg cursor-pointer duration-200 shadow-md hover:bg-main-black-800"
+        :class="{ 'bg-main-black-800': isShow, 'bg-main-black-900': !isShow }"
+        @click="$emit('toggleShow')"
+      >
+        <v-icon
+          :icon="isShow ? 'mdi-menu-up' : 'mdi-menu-down'"
+          size="large"
+          color="#fff"
+        />
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    num: Object,
+    isShow: Boolean,
+  });
+
+  const priceFormatter = (price) => {
+    const currency = new Intl.NumberFormat("en-us", {
+      style: "currency",
+      currency: "USD",
+    });
+
+    return currency.format(price).split(".")[0].split("$")[1];
+  };
+
+  const statusFormatter = (statusId) => {
+    return statusId === 1
+      ? "صفر به‌نام"
+      : statusId === 2
+      ? "صفر بی‌نام"
+      : statusId === 3
+      ? "کارکرده"
+      : statusId === 4
+      ? "در حد صفر"
+      : "";
+  };
+
+  const discountPercent = (price, discount) => {
+    const discountValue = price - discount;
+    const discountPercentage = (discountValue / price) * 100;
+    return discountPercentage.toFixed(0) + "%";
+  };
+</script>
+
+<style lang="scss" scoped></style>
