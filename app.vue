9d944a671fd22efcc1528847d390fff53ac0351c
Reza Darvishi <darvishireza8181@gmail.com>
2024-07-02 21:10:34 +0330

    finish app.vue file to show pages and components properly

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
 <template>
   <nuxt-layout name="default">
+    <the-search-toggle v-if="$route.path === '/'" />
+    <the-search-drawer v-if="$route.path === '/'" />
     <the-header />
+    <nuxt-layout name="container">
+      <nuxt-page />
+    </nuxt-layout>
+    <the-footer />
   </nuxt-layout>
 </template>
 
