900ffa3f91bbccd3465f1e80ff10fb48303e041b
Reza Darvishi <darvishireza8181@gmail.com>
2024-07-02 20:58:37 +0330

    add base components for multiple use case

---
+++ components/base/CheckboxSelect.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="w-full text-white">
+    <v-btn
+      v-ripple="false"
+      :append-icon="isOpen ? 'mdi-menu-up' : 'mdi-menu-down'"
+      @click="() => (isOpen = !isOpen)"
+      flat
+      color="white"
+      variant="plain"
+      size="large"
+      :text="label"
+      slim
+    >
+    </v-btn>
+    <div
+      class="w-full flex flex-col bg-main-black-800 rounded-lg shadow-md text-white duration-200"
+      :class="{
+        'px-4 py-2 h-[240px]': isOpen && label === 'پیش‌شماره',
+        'px-4 py-2 h-[180px]': isOpen,
+        'h-0 opacity-0': !isOpen,
+      }"
+    >
+      <div
+        class="px-2 flex flex-col items-end overflow-y-scroll"
+        style="direction: ltr"
+      >
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  const { label } = defineProps({
+    label: String,
+  });
+
+  const isOpen = ref(false);
+</script>
+
+<style scoped></style>
---
+++ components/base/ToggleButtons.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="w-max flex items-center justify-between rounded-xl bg-main-black-800"
+  >
+    <v-btn-toggle
+      v-model="selected"
+      variant="text"
+      @update:modelValue="emitUpdateValue"
+      density="compact"
+    >
+      <v-btn
+        v-for="(val, index) in values"
+        @click="() => (selected = index)"
+        flat
+        :class="{
+          'bg-transparent text-white': selected !== index,
+        }"
+        :color="selected === index ? '#ffda0a' : '#3d3d3d'"
+        rounded="xl"
+        :ripple="false"
+        :text="val"
+      >
+      </v-btn>
+    </v-btn-toggle>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    values: Array,
+  });
+  const emits = defineEmits(["updateValue"]);
+  const selected = ref(0);
+
+  const emitUpdateValue = (val) => {
+    emits("updateValue", val);
+  };
+</script>
+
+<style lang="scss" scoped></style>
