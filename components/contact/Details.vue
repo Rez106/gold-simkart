3916266de4940176d3b99068a34dad40b0ed88a2
Reza Darvishi <darvishireza8181@gmail.com>
2024-07-02 21:09:47 +0330

    complete contact page components

---
+++ components/contact/Details.vue
@@ -0,0 +1,94 @@
+<template>
+  <section
+    class="flex max-sm:flex-col max-sm:max-w-full items-center justify-between max-w-[90%] max-sm:gap-10 sm:mx-auto"
+  >
+    <article
+      class="max-w-[40%] max-sm:max-w-full leading-6 relative bg-main-black-800 px-5 py-10 rounded-lg shadow-lg shadow-main-black-950"
+    >
+      <div
+        class="absolute -top-4 -right-4 max-sm:-right-3 p-2 bg-gradient-to-tr from-main-black-900 to-main-black-800 shadow-md rounded-lg text-white font-semibold"
+      >
+        <v-icon icon="mdi-information" color="#fff" size="large" />
+        چرا <span class="text-main-yellow-400">مزایده</span> سیم‌کارت؟
+      </div>
+      <p
+        class="text-white text-wrap first-letter:mr-3 text-justify font-medium"
+      >
+        در <span class="text-main-yellow-400">مزایده</span> سیم‌کارت، ما مفتخریم
+        که مجموعه‌ای گسترده از سیم‌کارت‌ها را برای هر نوع نیازی ارائه می‌دهیم.
+        چه به دنبال سیم‌کارتی برای تماس‌های خود باشید و چه سیم‌کارتی برای کسب و
+        کار خود، ما گزینه‌های متعددی برای شما داریم. با
+        <span class="text-main-yellow-400">مزایده</span> سیم‌کارت، همواره
+        انتخاب‌های بیشتری در اختیار دارید.
+      </p>
+    </article>
+
+    <article
+      class="max-w-[40%] max-sm:max-w-full flex flex-col bg-main-black-800 p-5 rounded-lg shadow-lg shadow-main-black-950"
+    >
+      <div class="relative py-4">
+        <div
+          class="absolute top-1/3 -right-10 p-2 bg-gradient-to-tr from-main-black-900 to-main-black-800 shadow-md rounded-lg text-white font-semibold"
+        >
+          <v-icon
+            icon="mdi-phone"
+            color="#fff"
+            size="large"
+            style="transform: rotate(270deg)"
+          />
+        </div>
+        <div class="flex flex-col items-end gap-2">
+          <a
+            href="tel:+989010000550"
+            class="font-semibold duration-150 hover:text-white active:text-white text-main-black-300 text-2xl"
+            style="direction: ltr"
+          >
+            0901 0000 550
+          </a>
+          <a
+            href="tel:+989010000111"
+            class="font-semibold duration-150 hover:text-white active:text-white text-main-black-300 text-2xl"
+            style="direction: ltr"
+          >
+            0901 0000 111
+          </a>
+          <a
+            href="tel:+989131214444"
+            class="font-semibold duration-150 hover:text-white active:text-white text-main-black-300 text-2xl"
+            style="direction: ltr"
+          >
+            0913 121 4444
+          </a>
+        </div>
+      </div>
+      <v-divider class="my-5" />
+      <address
+        class="not-italic flex flex-col items-start gap-5 relative px-4 py-6"
+      >
+        <div
+          class="absolute top-1/3 -right-10 p-2 bg-gradient-to-tr from-main-black-900 to-main-black-800 shadow-md rounded-lg text-white font-semibold"
+        >
+          <v-icon icon="mdi-map-marker" color="#fff" size="large" />
+        </div>
+        <p class="text-white font-medium">
+          اصفهان، سیتی‌سنتر، طبقه دوم،
+          <span class="text-main-yellow-400">مزایده</span> سیم‌کارت
+        </p>
+        <v-btn
+          text="رفتن به نقشه"
+          prepend-icon="mdi-map-marker-path"
+          rounded="lg"
+          color="#6d6d6d"
+          target="_blank"
+          :v-ripple="false"
+          class="self-center"
+          href="https://www.google.com/maps/dir/?api=1&destination=32.551578, 51.689431"
+        />
+      </address>
+    </article>
+  </section>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ components/contact/Map.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="w-full flex items-center max-sm:flex-col justify-around">
+    <div class="sm:w-[30%] flex items-center sm:flex-col">
+      <h1 class="text-white text-3xl font-bold self-start">خدمات</h1>
+      <h1
+        class="text-main-yellow-400 text-3xl font-bold self-center underlined underline-overflow after:w-35"
+      >
+        حضوری
+      </h1>
+      <h1 class="underlined underline-overflow after:w-24"></h1>
+      <h1 class="underlined underline-overflow after:w-8"></h1>
+      <div class="self-end">
+        <v-icon icon="mdi-store" size="80" color="#fff" />
+      </div>
+    </div>
+    <div
+      class="w-full h-[200px] mt-5 2xl:h-[400px] 2xl:min-w-[600px] bg-white p-2 rounded-lg"
+    >
+      <LMap
+        :zoom="13"
+        :center="[32.551578, 51.689431]"
+        :use-global-leaflet="false"
+      >
+        <LTileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&amp;copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+          layer-type="base"
+          name="OpenStreetMap"
+        />
+        <LMarker :lat-lng="[32.551578, 51.689431]">
+          <LPopup> مزایده سیم‌کارت </LPopup>
+          <LIcon :icon-size="[50, 80]" class-name="leaflet">
+            <v-icon
+              icon="mdi-map-marker"
+              size="50"
+              class="text-main-black-700"
+            />
+          </LIcon>
+        </LMarker>
+      </LMap>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
