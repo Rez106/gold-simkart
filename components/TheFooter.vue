bc9d53bcee4aaf9ef347f7ad293ad4446f4e63dd
Reza Darvishi <darvishireza8181@gmail.com>
2024-07-12 14:24:41 +0330

    fix footer links

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -10,50 +10,70 @@
             <div
               class="flex py-4 items-center gap-1 font-semibold text-main-black-100"
             >
-              <v-icon icon="mdi-sim" color="#ffda0a" />
+              <v-icon :icon="mdiSim" color="#ffda0a" />
               <span>سیم‌کارت</span>
-            </div>
-            <ul
-              class="text-main-black-300 text-sm flex flex-col gap-1 [&>*]:underline-offset-2"
-            >
-              <li class="hover:underline cursor-pointer">خط‌های ایرانسل</li>
-              <li class="hover:underline cursor-pointer">خط‌های همراه اول</li>
-              <li class="hover:underline cursor-pointer">خط‌های 0912</li>
-              <li class="hover:underline cursor-pointer">خط‌های اقساطی</li>
-            </ul>
-          </div>
-          <div class="flex flex-col items-start">
-            <div
-              class="flex py-4 items-center gap-1 font-semibold text-main-black-100"
-            >
-              <v-icon icon="mdi-face-agent" color="#ffda0a" />
-              <span>ارتباط با ما</span>
             </div>
             <ul
               class="text-main-black-300 text-sm flex flex-col gap-1 [&>*]:underline-offset-2"
             >
               <li
                 class="hover:underline cursor-pointer"
-                @click="
-                  () => {
-                    router.push({
-                      path: '/contact',
-                    });
-                  }
-                "
+                @click="irancellHandler"
               >
-                حضوری
+                خط‌های ایرانسل
               </li>
-              <li class="hover:underline cursor-pointer">تماس</li>
-              <li class="hover:underline cursor-pointer">تلگرام</li>
-              <li class="hover:underline cursor-pointer">اینستاگرام</li>
+              <li class="hover:underline cursor-pointer" @click="hamrahHandler">
+                خط‌های همراه اول
+              </li>
+              <li
+                class="hover:underline cursor-pointer"
+                @click="preCodeHandler"
+              >
+                خط‌های 0912
+              </li>
+              <li
+                class="hover:underline cursor-pointer"
+                @click="categoryHandler"
+              >
+                خط‌های دائمی
+              </li>
+            </ul>
+          </div>
+          <div class="flex flex-col items-start">
+            <div
+              class="flex py-4 items-center gap-1 font-semibold text-main-black-100"
+            >
+              <v-icon :icon="mdiFaceAgent" color="#ffda0a" />
+              <span>ارتباط با ما</span>
+            </div>
+            <ul
+              class="text-main-black-300 text-sm flex flex-col gap-1 [&>*]:underline-offset-2"
+            >
+              <li class="hover:underline cursor-pointer">
+                <nuxt-link to="/contact">حضوری</nuxt-link>
+              </li>
+              <li class="hover:underline cursor-pointer">
+                <nuxt-link to="tel:09010000550"> تماس </nuxt-link>
+              </li>
+              <li class="hover:underline cursor-pointer">
+                <nuxt-link to="https://t.me/Gold_simkart" target="_blank"
+                  >تلگرام</nuxt-link
+                >
+              </li>
+              <li class="hover:underline cursor-pointer">
+                <nuxt-link
+                  to="https://www.instagram.com/mozayede_simkart2?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+                  target="_blank"
+                  >اینستاگرام</nuxt-link
+                >
+              </li>
             </ul>
           </div>
           <div class="">
             <div
               class="flex py-4 items-center gap-1 font-semibold text-main-black-100"
             >
-              <v-icon icon="mdi-dots-horizontal-circle" color="#ffda0a" />
+              <v-icon :icon="mdiDotsHorizontalCircle" color="#ffda0a" />
               <span>سایر</span>
             </div>
             <ul
@@ -97,5 +117,52 @@
 </template>
 
 <script setup>
+  import { mdiDotsHorizontalCircle, mdiFaceAgent, mdiSim } from "@mdi/js";
+
+  const filterStore = useFilterStore();
+  const { searchHandler, reset } = filterStore;
+  const {
+    selectedOperators,
+    selectedCategory,
+    selectedPreCode,
+    isAllOperators,
+    isAllPreCode,
+  } = storeToRefs(filterStore);
+
+  const { execute } = await useAsyncData(
+    "filter",
+    () => searchHandler().then(() => true),
+    {
+      immediate: false,
+      watch: false,
+    }
+  );
+
   const router = useRouter();
+
+  const irancellHandler = async () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    reset();
+    isAllOperators.value = false;
+    selectedOperators.value = [2];
+    await execute();
+  };
+  const hamrahHandler = async () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    reset();
+    isAllOperators.value = false;
+    selectedOperators.value = [1];
+    await execute();
+  };
+  const preCodeHandler = async () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    isAllPreCode.value = false;
+    selectedPreCode.value = ["0912"];
+    await execute();
+  };
+  const categoryHandler = async () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    selectedCategory.value = 1;
+    await execute();
+  };
 </script>
