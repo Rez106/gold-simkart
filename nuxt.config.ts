6ed9234bdef781035716660f0c25601998d94ef6
Reza Darvishi <darvishireza8181@gmail.com>
2024-07-12 14:23:59 +0330

    add mdi svg icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: [
@@ -23,7 +24,7 @@ export default defineNuxtConfig({
         fallback: "en",
       },
       icons: {
-        defaultSet: "mdi",
+        defaultSet: "mdi-svg",
       },
     },
   },
