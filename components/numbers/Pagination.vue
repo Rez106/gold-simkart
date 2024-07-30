8c71df1e5728b32772f751574edad463e4555d42
Reza Darvishi <darvishireza8181@gmail.com>
2024-07-30 19:40:51 +0330

    fix visible numbers in pagination

--- components/numbers/Pagination.vue
+++ components/numbers/Pagination.vue
@@ -1,16 +1,18 @@
 <template>
   <div
-    class="w-full flex items-center justify-center text-white"
+    class="w-full flex items-center justify-center text-white px-4"
     v-if="length > 0"
   >
     <v-pagination
       :length
       :next-icon="mdiMenuRight"
       :prev-icon="mdiMenuLeft"
-      :total-visible="4"
+      :total-visible="$vuetify.display.mobile ? 5 : 7"
       v-model="pageNumber"
       @update:modelValue="onPageUpdate"
       rounded="lg"
+      variant="outline"
+      :density="$vuetify.display.mobile ? 'compact' : undefined"
     ></v-pagination>
   </div>
 </template>
