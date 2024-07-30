b24e14f2ca481e48cc62823fc04b9af0dc0c1e87
Reza Darvishi <darvishireza8181@gmail.com>
2024-07-30 19:43:06 +0330

    change types component to use a brand new toggle button to fix the reactivity problem

--- components/sidebar/Types.vue
+++ components/sidebar/Types.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="w-full flex flex-col gap-3">
     <h1 class="text-white font-semibold px-2">نوع شماره</h1>
-    <base-toggle-buttons
-      :values="['همه', 'دائمی', 'اعتباری']"
-      class="self-center"
-      @updateValue="updateValue"
-    />
+    <div
+      class="w-max flex items-center justify-between rounded-xl bg-main-black-800 mx-auto"
+    >
+      <v-btn-toggle v-model="selectedCategory" variant="text" density="compact">
+        <v-btn
+          v-for="(val, index) in values"
+          flat
+          :class="{
+            'bg-transparent text-white': selectedCategory !== index,
+          }"
+          :color="selectedCategory === index ? '#ffda0a' : '#3d3d3d'"
+          rounded="xl"
+          :ripple="false"
+          :text="val"
+          :size="$vuetify.display.xs ? 'default' : undefined"
+        >
+        </v-btn>
+      </v-btn-toggle>
+    </div>
   </div>
 </template>
 
@@ -13,13 +27,7 @@
   const filterStore = useFilterStore();
   const { selectedCategory } = storeToRefs(filterStore);
 
-  const updateValue = (val) => {
-    if (val === 0) {
-      selectedCategory.value = null;
-    } else {
-      selectedCategory.value = val;
-    }
-  };
+  const values = ["همه", "دائمی", "اعتباری"];
 </script>
 
 <style lang="scss" scoped></style>
