b9be5607d90f87eebe5e29a22225f1fe49ed7d7a
Reza Darvishi <darvishireza8181@gmail.com>
2024-09-04 23:56:07 +0330

    chore/style(main.css,Numbers): change background and padding styles for the digit inputs and add main colors as a css variable in main.css

--- components/sidebar/Numbers.vue
+++ components/sidebar/Numbers.vue
@@ -2,76 +2,71 @@
   <div class="mt-3 flex flex-col items-center">
     <h1 class="text-white font-semibold px-2 self-start">شماره</h1>
     <client-only>
-      <div class="flex items-center flex-row-reverse gap-2 mt-5">
+      <div
+        class="w-full flex justify-end items-center flex-row-reverse gap-2 mt-5"
+      >
         <input
           id="inputNumber1"
           v-model="inputs.input1"
-          style="direction: ltr !important"
           @input="handleInput($event, 'input1')"
           type="number"
           maxlength="1"
           pattern="[0-9]"
-          class="p-1 bg-white rounded-md max-w-8 text-center"
+          class="number-input border !border-main-black-500 focus:!border-white"
         />
         <input
           id="inputNumber2"
           v-model="inputs.input2"
-          style="direction: ltr !important"
           @input="handleInput($event, 'input2')"
           type="number"
           maxlength="1"
           pattern="[0-9]"
-          class="p-1 bg-white rounded-md max-w-8 text-center"
+          class="number-input border !border-main-black-500 focus:!border-white"
         />
         <input
           id="inputNumber3"
           v-model="inputs.input3"
-          style="direction: ltr !important"
           @input="handleInput($event, 'input3')"
           type="number"
           maxlength="1"
           pattern="[0-9]"
-          class="p-1 bg-white rounded-md max-w-8 text-center"
+          class="number-input border !border-main-black-500 focus:!border-white"
         />
         <input
           id="inputNumber4"
           v-model="inputs.input4"
-          style="direction: ltr !important"
           @input="handleInput($event, 'input4')"
           type="number"
           maxlength="1"
           pattern="[0-9]"
-          class="p-1 bg-white rounded-md max-w-8 text-center"
+          class="number-input border !border-main-black-500 focus:!border-white"
         />
         <input
           id="inputNumber5"
           v-model="inputs.input5"
-          style="direction: ltr !important"
           @input="handleInput($event, 'input5')"
           type="number"
           maxlength="1"
           pattern="[0-9]"
-          class="p-1 bg-white rounded-md max-w-8 text-center"
+          class="number-input border !border-main-black-500 focus:!border-white"
         />
         <input
           id="inputNumber6"
           v-model="inputs.input6"
-          style="direction: ltr !important"
           @input="handleInput($event, 'input6')"
           type="number"
           maxlength="1"
           pattern="[0-9]"
-          class="p-1 bg-white rounded-md max-w-8 text-center"
+          class="number-input border !border-main-black-500 focus:!border-white"
         />
         <input
           id="inputNumber7"
           v-model="inputs.input7"
-          style="direction: ltr !important"
           @input="handleInput($event, 'input7')"
           type="number"
           maxlength="1"
           pattern="[0-9]"
-          class="p-1 bg-white rounded-md max-w-8 text-center"
+          class="number-input border !border-main-black-500 focus:!border-white"
         />
       </div>
     </client-only>
@@ -90,4 +85,9 @@
   };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+  .number-input {
+    direction: ltr !important;
+    @apply duration-150 max-xl:p-1 xl:p-1.5 bg-main-black-800 text-white outline-none rounded-md max-xl:w-7 xl:w-9 text-center;
+  }
+</style>
