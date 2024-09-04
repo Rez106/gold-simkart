2af51fff1cc431479e257b2179e2ca74b855824c
Reza Darvishi <darvishireza8181@gmail.com>
2024-09-04 23:56:45 +0330

    chore/update(package): update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  compatibilityDate: "2024-09-04",
   devtools: { enabled: false },
   modules: [
     "@nuxtjs/tailwindcss",
